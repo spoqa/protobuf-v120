7d4bcda889391eff899ab405ac50597f743db1df
Kevin Smyth <kevin.m.smyth@gmail.com>
2013-12-16 15:39:16 -0600

    NuGet autopkgs

---
+++ build_autopkgs.cmd
@@ -0,0 +1,6 @@
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe vsprojects\protobuf.sln /t:libprotobuf /p:Configuration=Release;Platform=x64
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe vsprojects\protobuf.sln /t:libprotobuf /p:Configuration=Debug;Platform=x64
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe vsprojects\protobuf.sln /t:libprotobuf /p:Configuration=Release;Platform=win32
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe vsprojects\protobuf.sln /t:libprotobuf /p:Configuration=Debug;Platform=win32
+powershell -ExecutionPolicy ByPass Write-NuGetPackage .\protobuf.autopkg
+powershell -ExecutionPolicy ByPass Write-NuGetPackage .\protobuf-vc100.autopkg
