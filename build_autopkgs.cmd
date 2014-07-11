3c4dee8686460c10501a62d92357920136e2092f
Swen Mun <longfin@spoqa.com>
2014-07-11 21:55:14 +0900

    Upgrade to vc110

--- build_autopkgs.cmd
+++ build_autopkgs.cmd
@@ -3,4 +3,4 @@ C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe vsprojects\protobuf.sl
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe vsprojects\protobuf.sln /t:libprotobuf /p:Configuration=Release;Platform=win32
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe vsprojects\protobuf.sln /t:libprotobuf /p:Configuration=Debug;Platform=win32
 powershell -ExecutionPolicy ByPass Write-NuGetPackage .\protobuf.autopkg
-powershell -ExecutionPolicy ByPass Write-NuGetPackage .\protobuf-vc100.autopkg
+powershell -ExecutionPolicy ByPass Write-NuGetPackage .\protobuf-vc110.autopkg
